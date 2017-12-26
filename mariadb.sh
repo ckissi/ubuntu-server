b651437b48cac8753a94f4a19bffaf037ab265ca
Csaba Kissi <ckissi@gmail.com>
2017-12-26 21:01:56 +0100

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -2,6 +2,18 @@
 # Settings for MariaDB
 
 DB_ROOT=`</dev/urandom tr -dc '1234567890!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB'| (head -c $1 > /dev/null 2>&1 || head -c 15)`
+DB_NAME=`</dev/urandom tr -dc a-z0-9| (head -c $1 > /dev/null 2>&1 || head -c 8)`
+DB_USER=`</dev/urandom tr -dc a-z0-9| (head -c $1 > /dev/null 2>&1 || head -c 8)`
+DB_PASSWORD=`</dev/urandom tr -dc A-Za-z0-9| (head -c $1 > /dev/null 2>&1 || head -c 10)`
+echo ""
+CONFIG_DIR=$PWD
+#Setup settings.txt for MySQL
+    sed -i "s/DB_ROOT/$DB_ROOT/g" $CONFIG_DIR/settings.txt
+    sed -i "s/DB_NAME/$DB_NAME/g" $CONFIG_DIR/settings.txt
+    sed -i "s/DB_USER/$DB_USER/g" $CONFIG_DIR/settings.txt
+    sed -i "s/DB_PASSWORD/$DB_PASSWORD/g" $CONFIG_DIR/settings.txt
+    sed -i "s/DB_HOSTNAME/$DB_HOSTNAME/g" $CONFIG_DIR/settings.txt
+    sed -i "s/DB_PORT/$DB_PORT/g" $CONFIG_DIR/settings.txt
 
 echo "Securing MariaDB... "
 sleep 5
