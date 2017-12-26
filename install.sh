8eb0a97a22c60c6f2efd4b41d1de285f88d36505
Csaba Kissi <ckissi@gmail.com>
2017-12-26 22:57:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ scriptPath=$PWD
 # add php7 and nginx repos
 sudo add-apt-repository -y ppa:nginx/development
 sudo add-apt-repository -y ppa:ondrej/php
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 sudo apt-get update
 
 # install basics
@@ -24,6 +25,13 @@ sudo apt-get install -y nginx
 echo "Installing PHP 7.1"
 sudo apt-get install -y php7.1-fpm php7.1-cli php7.1-mcrypt php7.1-gd php7.1-mysql php7.1-pgsql php7.1-imap php-memcached php7.1-mbstring php7.1-xml php7.1-curl php7.1-bcmath php7.1-sqlite3 php7.1-xdebug
 
+sudo apt-get install -y php5.6-fpm php5.6-cli php5.6-mcrypt php5.6-gd php5.6-mysql php5.6-xml php5.6-curl php5.6-mbstring
+
 # Composer
 echo "Installing Composer"
 php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer
+
+
+#MariaDB
+echo "Installing MariaDB"
+/bin/bash $scriptPath/mariadb.sh
