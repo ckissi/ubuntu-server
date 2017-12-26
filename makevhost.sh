9fa7d33b2bb3e4a7e4aa14b6ea6f1ef7a55922f4
Csaba Kissi <ckissi@gmail.com>
2017-12-26 23:52:44 +0100

    Create makevhost.sh

---
+++ makevhost.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Please enter domain name: "
+read domain
+#echo "You entered: $domain"
+
+mkdir /home/$domain
+mkdir /home/$domain/logs
+#mkdir /home/$domain/cgi-bin
+mkdir /home/$domain/public
+
+touch /etc/nginx/sites-available/$domain
+echo '
+server {
+    listen 80;
+    root /home/$domain/public;
+    index index.html index.htm index.php;
+    server_name $domain;
+    location / {
+        try_files $uri $uri/ /index.php$is_args$args;
+    }
+    location ~ \.php$ {
+       include snippets/fastcgi-php.conf;
+       fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
+    }
+}
+' > /etc/nginx/sites-available/$domain
+
+# enable vhost
+nginx_ensite $domain
+
+#reload nginx
+service nginx reload
