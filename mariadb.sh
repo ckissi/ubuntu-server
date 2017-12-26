25e3371475b25b87d312fe8e8260eea229dcd73c
Csaba Kissi <ckissi@gmail.com>
2017-12-26 21:51:26 +0100

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -45,7 +45,7 @@ expect -f - <<-EOF
   expect eof
 EOF
 
-mysql -u USER -p$DB_ROOT <<EOF
+mysql -u root -p$DB_ROOT <<EOF
 use mysql
 update user set plugin='mysql_native_password' where user='root'
 flush privileges
