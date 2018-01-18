ceea7398f3aed79a321bb6818f14d93f8e475506
Csaba Kissi <ckissi@gmail.com>
2018-01-18 23:28:23 +0100

    Update makevhost.sh

--- makevhost.sh
+++ makevhost.sh
@@ -45,6 +45,7 @@ server {
     location ~ \.php$ {
        include snippets/fastcgi-php.conf;
        fastcgi_pass unix:/run/php/php7.1-fpm.sock;
+      #fastcgi_pass unix:/run/php/php5.6-fpm.sock; 
     }
 }
 EOF
