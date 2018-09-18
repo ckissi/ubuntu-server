f28abd3630a8bce4729558fe0a08ce2ae74a9457
Csaba Kissi <ckissi@gmail.com>
2018-09-18 09:47:20 +0200

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -28,6 +28,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
 sudo apt-get install -y mariadb-server mariadb-client
+mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('');"
 
 echo "Securing MariaDB... "
 sleep 5
